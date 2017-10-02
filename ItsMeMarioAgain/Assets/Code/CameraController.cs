6588134d80cdc6a70997fe8a6b9183ab11282a15
Timo van Dijken <timovandijken@outlook.com>
2017-10-02 15:02:51 +0200

    first commit after losing ssh key :(

---
+++ ItsMeMarioAgain/Assets/Code/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+  public Transform character;
+  public Player player;
+  private Vector3 offset;
+  private Vector3 startPosition;
+
+  //todo Reset camera when the player dies
+
+  // Use this for initialization
+  void Start()
+  {
+    startPosition = transform.position;
+    offset = transform.position - character.transform.position;
+  }
+  public void Reset()
+  {
+    transform.position = startPosition;
+  }
+
+  // updates after all the other updates
+  void LateUpdate()
+  {
+
+    if (character.position.x - transform.position.x > 0)
+    {
+      // Moves the camera with Mario (does not move in the Y-Axis) sets a height of 7 for the camera
+      Vector3 targetCamPos = new Vector3(character.position.x + offset.x, transform.position.y, transform.position.z);
+      transform.position = targetCamPos;
+    }
+  }
+}
---
+++ ItsMeMarioAgain/Assets/Code/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public float speed;
+  private Rigidbody2D rb2d;
+  bool grounded;
+  public float jumpSpeed;
+  public CameraController cameraController;
+
+  void Awake()
+  {
+    rb2d = GetComponent<Rigidbody2D>();
+  }
+
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.transform.tag == "Ground")
+    {
+      grounded = true;
+    }
+  }
+  void Die()
+  {
+    transform.position = new Vector3(0f, 1.5f, 0f);
+    cameraController.Reset();
+    Debug.Log("you died");
+  }
+
+    void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    rb2d.AddForce(movement * speed);
+
+    if (grounded)
+    {
+      if (Input.GetKey(KeyCode.Space))
+      {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed);
+        grounded = false;
+      }
+    }
+  }
+}
